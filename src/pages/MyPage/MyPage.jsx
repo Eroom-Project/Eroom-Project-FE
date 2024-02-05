d8d4f8e54e554573ca578ffec3d4b5e91b6c7cc0
CHOI WOO SIK <rhrod123456@gmail.com>
2024-02-05 20:14:36 +0900

    sign

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import Isometric from '../../components/Common/Isometric'
+import styled from 'styled-components'
 
 function MyPage() {
     return (
-        <div>
-            <Isometric />
-        </div>
+        <MainBox>
+            <Profile>
+
+            </Profile>
+            <Isometric/>
+        </MainBox>
     )
 }
 
 export default MyPage
+
+const MainBox =styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+`
+const ProfileBox = styled.div`
+    
+`
+const Profile = styled.div`
+    display: flex;
+`
